486fdbae2571e644be5ac7ac1e232c12c7cc1d80
l.jiang <ljiang510@gmail.com>
2018-10-09 15:45:43 -0700

    two more projects

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/', express.static(path.join(__dirname, '/')));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
+app.get('', (req, res) => {
   res.sendFile(path.join(__dirname, 'projects.html'));
 })
 
